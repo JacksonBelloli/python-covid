f7722051c5684cd94a0952bcbf433ed43c633129
JacksonBelloli <jakson.belloli@unesc.net>
2021-08-06 13:35:07 -0300

    otmized

--- app/controllers/controllerAmrec.py
+++ app/controllers/controllerAmrec.py
@@ -1,93 +1,90 @@
 from flask import Flask
 import csv
 import json
+import pandas as pd
+from itertools import islice
 from app.controllers.controllerThread import Thread
+
 app = Flask(__name__)
 
 
 class controllerAmrec:
     cities = [
-            {
-                'name':'Criciúma',
-                'population': 215186,
-                'cod': 4204608
-            },
-            {
-                'name':'Balneário Rincão',
-                'population': 12760,
-                'cod': 4220000
-            },
-            {
-                'name':'Cocal do Sul',
-                'population':16684,
-                'cod': 4204251
-            },
-            {
-                'name':'Morro da Fumaça',
-                'population':17796,
-                'cod': 4211207
-            },
-            {
-                'name':'Siderópolis',
-                'population':14007,
-                'cod': 4217600
-            },
-            {
-                'name':'Nova Veneza',
-                'population':15166,
-                'cod': 4211603
-            },
-            {
-                'name':'Treviso',
-                'population':3600,
-                'cod': 4218350
-            },
-            {
-                'name':'Orleans',
-                'population':22912,
-                'cod': 4211702
-            },
-            {
-                'name':'Urussanga',
-                'population':21268,
-                'cod': 4219002
-            },
-            {
-                'name':'Forquilinha',
-                'population':25988,
-                'cod': 4205456
-            },
-            {
-                'name':'Lauro Müller',
-                'population':13359,
-                'cod': 4209607
-            },
-            {
-                'name':'Içara',
-                'population':56421,
-                'cod': 4207007
-            }
-        ]
-    data = []
+        {
+            'name': 'Criciúma',
+            'population': 215186,
+            'cod': 4204608
+        },
+        {
+            'name': 'Balneário Rincão',
+            'population': 12760,
+            'cod': 4220000
+        },
+        {
+            'name': 'Cocal do Sul',
+            'population': 16684,
+            'cod': 4204251
+        },
+        {
+            'name': 'Morro da Fumaça',
+            'population': 17796,
+            'cod': 4211207
+        },
+        {
+            'name': 'Siderópolis',
+            'population': 14007,
+            'cod': 4217600
+        },
+        {
+            'name': 'Nova Veneza',
+            'population': 15166,
+            'cod': 4211603
+        },
+        {
+            'name': 'Treviso',
+            'population': 3600,
+            'cod': 4218350
+        },
+        {
+            'name': 'Orleans',
+            'population': 22912,
+            'cod': 4211702
+        },
+        {
+            'name': 'Urussanga',
+            'population': 21268,
+            'cod': 4219002
+        },
+        {
+            'name': 'Forquilinha',
+            'population': 25988,
+            'cod': 4205456
+        },
+        {
+            'name': 'Lauro Müller',
+            'population': 13359,
+            'cod': 4209607
+        },
+        {
+            'name': 'Içara',
+            'population': 56421,
+            'cod': 4207007
+        }
+    ]
+    data = pd.DataFrame()
     path = 'app/file/caso_full.csv'
 
     def __init__(self):
         print('init')
 
-    def filter_csv(self):
-        with open(self.path, newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                 if row['city_ibge_code'] == '':
-                     continue
-                 if row['state'] != 'SC':
-                     continue
-                 for city in self.cities:
-                     if int(row['city_ibge_code']) == city['cod']:
-                         self.data.append(row)
 
     def get_data(self):
-        thread = Thread(self.filter_csv)
-        thread.start()
-        thread.join()
-        return json.dumps(self.data)
+        df = pd.read_csv(self.path, header=0)
+
+        for city in self.cities:
+            newdf = df[(df.city_ibge_code == city['cod'])]
+            self.data = pd.concat([self.data, newdf])
+
+        return self.data.to_json(orient='records')
+        #return json.dumps(self.data)
+
--- app/controllers/controllerThread.py
+++ app/controllers/controllerThread.py
@@ -3,9 +3,10 @@ import time
 
 
 class Thread (threading.Thread):
-    def __init__(self, function):
+    def __init__(self, function, param):
         threading.Thread.__init__(self)
         self.function = function
+        self.param = param
 
     def run(self):
-        self.function()
+        self.function(self.param)
