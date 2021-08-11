6999188ef7b387ca28265e4950909ff53e93df39
JacksonBelloli <jakson.belloli@unesc.net>
2021-08-11 16:10:11 -0300

    Sc and Top 10 cities

---
+++ app/controllers/controllerTop10.py
@@ -0,0 +1,83 @@
+from flask import Flask
+import csv
+import json
+import pandas as pd
+from datetime import date, timedelta
+from app.controllers.controllerThread import Thread
+
+app = Flask(__name__)
+
+
+class controllerTop10:
+    cities = [
+        {
+            'name' : 'Joinville',
+            'population' : 590466
+        },
+        {
+            'name' : 'Florianópolis',
+            'population' : 500973
+        },
+        {
+            'name' : 'Blumenau',
+            'population' : 357199
+        },
+        {
+            'name' : 'São José',
+            'population' : 246586
+        },
+        {
+            'name' : 'Chapecó',
+            'population' : 166040
+        },
+        {
+            'name' : 'Itajaí',
+            'population' : 219536
+        },
+        {
+            'name' : 'Criciúma',
+            'population' : 215186
+        },
+        {
+            'name' : 'Jaraguá do Sul',
+            'population' : 177697
+        },
+        {
+            'name' : 'Palhoça',
+            'population' : 171797
+        },
+        {
+            'name' : 'Lages',
+            'population' : 157544
+        }
+    ]
+    data = pd.DataFrame()
+    path = 'app/file/caso_full.csv'
+
+    def __init__(self):
+        print('init')
+
+    def get_data(self):
+        yesterday = date.today() - timedelta(days=1)
+
+        yesterday = yesterday.strftime('%Y-%m-%d')
+
+        df = pd.read_csv(self.path, header=0)
+
+
+        for city in self.cities:
+            newdf = df[(df.city == city['name']) & (df.date == yesterday)]
+            self.data = pd.concat([self.data, newdf])
+        #self.data.to_csv('app/file/caso_teste.csv')
+        return self.data.to_json(orient='records')
+        #return json.dumps(self.data)
+
+    def get_data_daily(self):
+        df = pd.read_csv(self.path, header=0)
+
+        for city in self.cities:
+            newdf = df[(df.city == city['name'])]
+            self.data = pd.concat([self.data, newdf])
+        # self.data.to_csv('app/file/caso_teste.csv')
+        return self.data.to_json(orient='records')
+        # return json.dumps(self.data)
--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from flask import Flask
+import pandas as pd
 from app.controllers.controllerAmrec import *
 from app.controllers.controllerAmesc import *
 from app.controllers.controllerAmurel import *
+from app.controllers.controllerTop10 import *
 from app.data.data import Data
 app = Flask(__name__)
 thread = Data()
@@ -34,3 +36,15 @@ def amurel():
 def amurel_daily():
     amurelc = controllerAmurel()
     return amurelc.get_data_daily()
+@app.route('/api/sc', methods=['GET'])
+def sc():
+    df = pd.read_csv('app/file/caso_full.csv')
+    return df.to_json(orient='records')
+@app.route('/api/top10', methods=['GET'])
+def top10():
+    top10c = controllerTop10()
+    return top10c.get_data()
+@app.route('/api/top10/daily', methods=['GET'])
+def top10_daily():
+    top10c = controllerTop10()
+    return top10c.get_data_daily()
